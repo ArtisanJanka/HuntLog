c3431bc1b8b65c84ff66ed1b6341ff3b4c40bc53
JanisBramanisVTDTipb22 <127381689+ArtisanJanka@users.noreply.github.com>
2026-04-27 09:17:29 +0300

    fixed credentials error

---
+++ lang/lv/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Šie pieslēgšanās dati neatbilst mūsu ierakstiem.',
+    'password' => 'Norādītā parole nav pareiza.',
+    'throttle' => 'Pārāk daudz pieslēgšanās mēģinājumu. Lūdzu, mēģiniet vēlreiz pēc :seconds sekundēm.',
+];
